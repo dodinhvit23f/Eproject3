ef539c7086cbb63530b34c14e3119df8ef02f9c6
tu huu <Jaryhuu@gmail.com>
2020-10-24 20:19:34 +0700

    Commit de k mat code

--- Eproject3/Areas/Admin/Controllers/HomeController.cs
+++ Eproject3/Areas/Admin/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Eproject3.Areas.Admin.Models
+namespace Eproject3.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
@@ -19,27 +19,49 @@ namespace Eproject3.Areas.Admin.Models
         {
             if (Session["isAdmin"] == null)
             {
-                return RedirectToAction("LoginView", "Users");
+                return RedirectToAction("Login");
             }
             Users user = (Users)Session["uses"];
             ViewBag.Pack_id = new SelectList(db.Packs, "id", "name", user.Pack_id);
             ViewBag.Roll_id = new SelectList(db.Roles, "id", "name", user.Roll_id);
             return View(user);
         }
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Index([Bind(Include = "id,UPhone,UPass,UAdress,Img,Roll_id,Pack_id,Exp_Date,AccNum")] Users users)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        users.UPass = r.HashPwd(users.UPass);
-        //        db.Entry(users).State = EntityState.Modified;
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-        //    ViewBag.Pack_id = new SelectList(db.Packs, "id", "name", users.Pack_id);
-        //    ViewBag.Roll_id = new SelectList(db.Roles, "id", "name", users.Roll_id);
-        //    return View(users);
-        //}
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string usn,string pwd)
+        {
+            string hashed = r.HashPwd(pwd);
+            var isValid = db.Users.Where(p=>p.UPhone==usn && p.UPass== hashed).FirstOrDefault();
+            if (isValid == null)
+            {
+                ViewBag.err = "Wrong credential";
+                return View();
+            }else if (isValid.Roll_id != 1 )
+            {
+                ViewBag.err = "You are not permited here";
+                return View();
+            }
+            Session["uses"] = isValid;
+            Session["isAdmin"] = true;
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index([Bind(Include = "id,UPhone,UPass,UAdress,Img,Roll_id,Pack_id,Exp_Date,AccNum")] Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                users.UPass = r.HashPwd(users.UPass);
+                db.Entry(users).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Pack_id = new SelectList(db.Packs, "id", "name", users.Pack_id);
+            ViewBag.Roll_id = new SelectList(db.Roles, "id", "name", users.Roll_id);
+            return View(users);
+        }
     }
 }
